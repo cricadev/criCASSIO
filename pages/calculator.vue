b99bf31cc36be4ab635a92bd4ed9c47e89a22432
CristianN998 <cristiancasallas998@gmail.com>
2023-10-20 19:07:20 -0500

    SEO

--- pages/calculator.vue
+++ pages/calculator.vue
@@ -3,3 +3,11 @@
     <Calculator />
   </div>
 </template>
+<script setup>
+useHead({
+  title: 'CriCASSIO',
+  meta: [
+    { name: 'description', content: "The ultimate calculator bee yourself with this pleasingly and functional calculator." }
+  ],
+})
+</script>
